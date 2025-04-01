566988c82e373b021b9590f457f7f115d5da9204
Tone <mirandatone@outlook.com>
2025-04-01 19:03:04 -0300

    feat: create and use InternalServerError in api/v1/status

---
+++ infra/errors.js
@@ -0,0 +1,19 @@
+export class InternalServerError extends Error {
+  constructor({ cause }) {
+    super("Um erro interno não esperado ocorreu.", {
+      cause,
+    });
+    this.name = "InternalServerError";
+    this.actions = "Entre em contato com o suporte.";
+    this.statusCode = 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      actions: this.actions,
+      status_code: this.statusCode,
+    };
+  }
+}
