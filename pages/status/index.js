d0a2ff56fe0478a75658fc48379202227e8f5da6
Tone <mirandatone@outlook.com>
2025-04-01 09:03:58 -0300

    feat: add simple frontend for status page using `fetch status api`

---
+++ pages/status/index.js
@@ -0,0 +1,50 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status Page</h1>
+      <UpdatedAt />
+    </>
+  );
+}
+
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let updatedAtText = "Carregando...";
+  let updatedAtTextVersionDatabase = "Carregando...";
+  let updatedAtTextMaxConnectionsDatabase = "Carregando...";
+  let updatedAtTextOpenedConnectionsDatabase = "Carregando...";
+
+  if (!isLoading && data) {
+    updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+    updatedAtTextVersionDatabase = data.dependencies.database.version;
+    updatedAtTextOpenedConnectionsDatabase =
+      data.dependencies.database.opened_connections;
+    updatedAtTextMaxConnectionsDatabase =
+      data.dependencies.database.max_connections;
+  }
+
+  return (
+    <div>
+      Última atualização: {updatedAtText}
+      <h2>Banco de Dados</h2>
+      <ul>
+        <li>Versão:{updatedAtTextVersionDatabase}</li>
+        <li>Abertas Conexões: {updatedAtTextOpenedConnectionsDatabase}</li>
+        <li>Máxima Conexões: {updatedAtTextMaxConnectionsDatabase}</li>
+      </ul>
+      <hr></hr>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
