7699653763e0b15d5da06adabe2b6e211e55ae86
Tone <mirandatone@outlook.com>
2025-04-02 22:44:07 -0300

    feat: create users migration

---
+++ infra/migrations/1743642292953_create-users.js
@@ -0,0 +1,43 @@
+
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      notNull: true,
+      default: pgm.func("gen_random_uuid()")
+    },
+
+    // For reference, GitHub liits username to 39 characters.
+    username:{
+      type: "varchar(30)",
+      notNull: true,
+      unique: true,
+    },
+
+    //The maximum lenght of an email addres is 254 charaters.
+    email:{
+      type: "varchar(254)",
+      notNull: true,
+      unique: true,
+    },
+
+    password: {
+      type: "varchar(72)",
+      notNull: true,
+    },
+
+    create_at: {
+      type: "timestamptz",
+      default: pgm.func("now()"),
+    },
+
+    update_at: {
+      type: "timestamptz",
+      default: pgm.func("now()"),
+    },
+  });
+  
+};
+
+exports.down = false;
